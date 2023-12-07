c0dac2cff76ba38162dd1a7c11b9efb8e753809b
Denys Topchyi <miliarder97@gmail.com>
2023-12-07 18:33:09 +0200

    feat: add playvideo component

---
+++ src/app/components/PlayVideo/PlayVideo.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import playVideo from '../../../../public/icons/play-video-active.svg'
+import cn from 'classnames'
+
+interface PlayVideoProps {
+  classname: string
+  playVideoFunc: any
+}
+
+type PartialVideoProps = Partial<PlayVideoProps>
+
+export const PlayVideo: React.FC<PartialVideoProps> = ({ classname }) => {
+  return (
+    <Image
+      src={playVideo}
+      alt="playVideo"
+      className={cn('play-video', {
+        [`${classname}`]: !!classname
+      })}
+    />
+  )
+}
