ec3b11576183413e5f3eab91b296fb073d34bccf
Denys Topchyi <miliarder97@gmail.com>
2023-12-08 18:29:32 +0200

    feat: add global classes to the whole page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from './components/Header/Header'
+import './styles/page.scss'
 
 export default function Page() {
   return (
-    <main>
+    <>
       <Header />
-    </main>
+      <main>123123123</main>
+    </>
   )
 }
