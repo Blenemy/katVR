5839ac501eba4db83b08375cb355adad95c94a34
Denys Topchyi <miliarder97@gmail.com>
2023-12-09 20:32:28 +0200

    feat: add navbar links to i18n

---
+++ src/app/i18next/locales/de.ts
@@ -0,0 +1,19 @@
+export const de = {
+  translation: {
+    'header.title': 'DER NEUE BEGINN VON',
+    'header.textDecorator': 'VR-FORTBEWEGUNG',
+    'header.subtitle':
+      'Entdecken Sie das umfassendste VR-Fortbewegungssystem und erschließen Sie unendliche Bewegungsmöglichkeiten in allen Spielen auf allen Plattformen!',
+    'header.price': '1200$',
+    'header.button': 'Jetzt kaufen',
+    navbar: {
+      'navbar.about': 'Über uns',
+      'navbar.tech': 'Technik',
+      'navbar.benefits': 'Vorteile',
+      'navbar.contact': 'Kontakt',
+      'navbar.language': 'Sprache',
+      'navbar.faq': 'FAQ',
+      'navbar.help': 'Hilfe'
+    }
+  }
+}
---
+++ src/app/i18next/locales/en.ts
@@ -0,0 +1,19 @@
+export const en = {
+  translation: {
+    'header.title': 'THE NEW START OF',
+    'header.textDecorator': 'VR LOCOMOTION',
+    'header.subtitle':
+      'Discover the most comprehensive VR Locomotion system, and unlock infinite motion in any games on any platforms!',
+    'header.price': '1200$',
+    'header.button': 'Buy Now',
+    navbar: {
+      'navbar.about': 'About',
+      'navbar.tech': 'Tech',
+      'navbar.benefits': 'Benefits',
+      'navbar.contact': 'Contact',
+      'navbar.language': 'Language',
+      'navbar.faq': 'FAQ',
+      'navbar.help': 'Help'
+    }
+  }
+}
