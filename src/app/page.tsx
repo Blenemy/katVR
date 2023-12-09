e86f7dc0bde584a170a6e67c0f5926dad160c860
Denys Topchyi <miliarder97@gmail.com>
2023-12-09 21:17:46 +0200

    feat: add global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { Header } from './components/Header/Header'
 import './i18next/i18n'
 import './styles/page.scss'
+import './styles/globals.scss'
 
 export default function Page() {
   return (
