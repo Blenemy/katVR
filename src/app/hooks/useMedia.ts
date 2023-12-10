6aa43d862ae70fe85f93fae819e8e25e55f01f51
Denys Topchyi <miliarder97@gmail.com>
2023-12-10 16:17:05 +0200

    bug: delete useMediAQury hook

--- src/app/hooks/useMedia.ts
+++ src/app/hooks/useMedia.ts
@@ -1,32 +1,32 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 
-export const useMedia = () => {
-  const [isDesktop, setIsDesktop] = useState(false)
-  const [isTablet, setIsTablet] = useState(false)
-  const [isMobile, setIsMobile] = useState(false)
+// export const useMedia = () => {
+//   const [isDesktop, setIsDesktop] = useState(false)
+//   const [isTablet, setIsTablet] = useState(false)
+//   const [isMobile, setIsMobile] = useState(false)
 
-  useEffect(() => {
-    const checkDesktop = window.matchMedia('(min-width: 1280px)').matches
-    const checkTablet = window.matchMedia(
-      '(min-width: 768px) and (max-width: 1279px)'
-    ).matches
-    const checkMobile = window.matchMedia('(max-width: 767px)').matches
+//   useEffect(() => {
+//     const checkDesktop = window.matchMedia('(min-width: 1280px)').matches
+//     const checkTablet = window.matchMedia(
+//       '(min-width: 768px) and (max-width: 1279px)'
+//     ).matches
+//     const checkMobile = window.matchMedia('(max-width: 767px)').matches
 
-    setIsDesktop(checkDesktop)
-    setIsTablet(checkTablet)
-    setIsMobile(checkMobile)
+//     setIsDesktop(checkDesktop)
+//     setIsTablet(checkTablet)
+//     setIsMobile(checkMobile)
 
-    const handleResize = () => {
-      setIsDesktop(window.matchMedia('(min-width: 1280px)').matches)
-      setIsTablet(
-        window.matchMedia('(min-width: 768px) and (max-width: 1279px)').matches
-      )
-      setIsMobile(window.matchMedia('(max-width: 767px)').matches)
-    }
+//     const handleResize = () => {
+//       setIsDesktop(window.matchMedia('(min-width: 1280px)').matches)
+//       setIsTablet(
+//         window.matchMedia('(min-width: 768px) and (max-width: 1279px)').matches
+//       )
+//       setIsMobile(window.matchMedia('(max-width: 767px)').matches)
+//     }
 
-    window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
-  }, [])
+//     window.addEventListener('resize', handleResize)
+//     return () => window.removeEventListener('resize', handleResize)
+//   }, [])
 
-  return { isDesktop, isTablet, isMobile }
-}
+//   return { isDesktop, isTablet, isMobile }
+// }
