11c4d8683370f64c04a5ae0ab2e4cdfe8b131596
Denys Topchyi <miliarder97@gmail.com>
2023-12-10 16:17:17 +0200

    feat: add language selector dropdown

---
+++ src/app/components/Header/LanguageDropdown/LanguageDropdown.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { ChangeEvent, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import './LanguageDropdown.scss'
+
+export const LanguageDropdown = () => {
+  const { i18n } = useTranslation()
+  const [selectedOption, setSelectedOption] = useState('En')
+
+  const handleOnChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedOption(e.target.value)
+    i18n.changeLanguage(e.target.value)
+    console.log(i18n)
+  }
+
+  return (
+    <select
+      value={selectedOption}
+      onChange={(e) => handleOnChange(e)}
+      className="language-dropdown"
+    >
+      <option className="language-dropdown__option" value="en">
+        En
+      </option>
+      <option className="language-dropdown__option" value="de">
+        Ge
+      </option>
+    </select>
+  )
+}
