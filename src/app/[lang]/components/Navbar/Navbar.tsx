739dc7901015877d85d7afa8feb57190e66a38b5
Denys Topchyi <miliarder97@gmail.com>
2023-12-12 18:15:08 +0200

    fix: improved navbar component, use use client

--- src/app/[lang]/components/Navbar/Navbar.tsx
+++ src/app/[lang]/components/Navbar/Navbar.tsx
@@ -1,23 +1,26 @@
+'use client'
+
 import cn from 'classnames'
 import Image from 'next/image'
-import { useTranslation } from 'react-i18next'
-
-import { menuItems } from './navLinks'
 
 import cross from '../../../../../public/icons/cross-white.svg'
 
 import './Navbar.scss'
+import { toggleNavbar } from '@/lib/features/navbarSlice'
+import { useAppDispatch, useAppSelector } from '@/lib/hooks'
+import { Translations } from '../../types/Translations'
 
 interface NavbarProps {
-  handleOpenNavbar: () => void
-  isOpened: boolean
+  t: Translations
 }
 
-export const Navbar: React.FC<NavbarProps> = ({
-  handleOpenNavbar,
-  isOpened
-}) => {
-  const { t } = useTranslation()
+export const Navbar: React.FC<NavbarProps> = ({ t }) => {
+  const { isOpened } = useAppSelector((state) => state.navbar)
+  const dispatch = useAppDispatch()
+
+  const handleOnClick = () => {
+    dispatch(toggleNavbar(!isOpened))
+  }
 
   return (
     <nav
@@ -30,16 +33,44 @@ export const Navbar: React.FC<NavbarProps> = ({
             src={cross}
             alt="burgerImage"
             className="menu__close"
-            onClick={handleOpenNavbar}
+            onClick={handleOnClick}
           />
           <ul className="menu__list">
-            {menuItems.map((item, index) => (
-              <li key={index} className="menu__item">
-                <a href={item.href} className="menu__link">
-                  {t(`navbar.${item.i18nKey}`)}
-                </a>
-              </li>
-            ))}
+            <li className="menu__item">
+              <a href={'/'} className="menu__link">
+                {t.navbar.about}
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href={'/'} className="menu__link">
+                {t.navbar.tech}
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href={'/'} className="menu__link">
+                {t.navbar.benefits}
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href={'/'} className="menu__link">
+                {t.navbar.contact}
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href={'/'} className="menu__link">
+                {t.navbar.language}
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href={'/'} className="menu__link">
+                {t.navbar.faq}
+              </a>
+            </li>
+            <li className="menu__item">
+              <a href={'/'} className="menu__link">
+                {t.navbar.help}
+              </a>
+            </li>
           </ul>
         </div>
       </div>
--- src/app/[lang]/components/Navbar/navLinks.ts
+++
@@ -1,9 +0,0 @@
-export const menuItems = [
-  { href: '#', i18nKey: 'navbar.about', text: 'About' },
-  { href: '#features', i18nKey: 'navbar.tech', text: 'Tech' },
-  { href: '#materials', i18nKey: 'navbar.benefits', text: 'Benefits' },
-  { href: '#meet-us', i18nKey: 'navbar.contact', text: 'Contact' },
-  { href: '#footer', i18nKey: 'navbar.language', text: 'Language' },
-  { href: '#footer', i18nKey: 'navbar.faq', text: 'FAQ' },
-  { href: '#footer', i18nKey: 'navbar.help', text: 'Help' }
-]
