daa6a1811aadf1831cfb57680bac4b2bd396866e
Denys Topchyi <miliarder97@gmail.com>
2023-12-12 18:14:22 +0200

    bug: changed to use client component

--- src/app/[lang]/components/Header/BurgerIcon/BurgerIcon.tsx
+++ src/app/[lang]/components/Header/BurgerIcon/BurgerIcon.tsx
@@ -1,23 +1,27 @@
 'use client'
 
+import { toggleNavbar } from '@/lib/features/navbarSlice'
+import { useAppDispatch, useAppSelector } from '@/lib/hooks'
 import Image from 'next/image'
 
-interface BurgerIconProps {
-  src: string
-  handleOpenNavbar: () => void
-}
+import burgerImage from '../../../../../../public/icons/burger.svg'
+import cross from '../../../../../../public/icons/cross-white.svg'
 
-export const BurgerIcon: React.FC<BurgerIconProps> = ({
-  src,
-  handleOpenNavbar
-}) => {
+export const BurgerIcon: React.FC = () => {
+  const { isOpened } = useAppSelector((state) => state.navbar)
+  const dispatch = useAppDispatch()
+
+  const handleOnClick = () => {
+    dispatch(toggleNavbar(!isOpened))
+  }
+
   return (
     <div className="header__logo header__logo_navbar">
       <Image
-        src={src}
         alt="burgerImage"
         className="header__img"
-        onClick={handleOpenNavbar}
+        onClick={handleOnClick}
+        src={isOpened ? cross : burgerImage}
       />
     </div>
   )
