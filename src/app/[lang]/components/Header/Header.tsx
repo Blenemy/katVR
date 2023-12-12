0b17c5ef8270df08023806c29b0b70a686c075e7
Denys Topchyi <miliarder97@gmail.com>
2023-12-12 17:33:59 +0200

    fix: fix header comp to use i18n with ssr

--- src/app/[lang]/components/Header/Header.tsx
+++ src/app/[lang]/components/Header/Header.tsx
@@ -1,40 +1,30 @@
 import Image from 'next/image'
-import { useAppDispatch, useAppSelector } from '@/lib/hooks'
-import { useTranslation } from 'react-i18next'
-import { toggleNavbar } from '@/lib/features/navbarSlice'
 
 import { Button } from '../Button/Button'
 import { TextDecorator } from '../TextDecorator/TextDecorator'
 import { PlayVideo } from '../PlayVideo/PlayVideo'
-import { Navbar } from '../Navbar/Navbar'
+// import { Navbar } from '../Navbar/Navbar'
 import { ControlBar } from './ControlBar/ControlBar'
 import { LanguageDropdown } from './LanguageDropdown/LanguageDropdown'
 import { Container } from '../Container/Container'
 
-import logo from '../../../../public/icons/Logo.svg'
-import burgerImage from '../../../../public/icons/burger.svg'
-import crossImage from '../../../../public/icons/cross-white.svg'
+import logo from '../../../../../public/icons/Logo.svg'
+import burgerImage from '../../../../../public/icons/burger.svg'
+// import crossImage from '../../../../../public/icons/cross-white.svg'
 import mainImage_2x from './images/header-main_2x.png'
 
 import './Header.scss'
-import { useEffect } from 'react'
+import { BurgerIcon } from './BurgerIcon/BurgerIcon'
 
-export const Header = () => {
-  const dispatch = useAppDispatch()
-  const { isOpened } = useAppSelector((state) => state.navbar)
-  const { t } = useTranslation()
+interface HeaderProps {
+  t: any
+}
 
-  const handleOpenNavbar = () => {
-    dispatch(toggleNavbar(!isOpened))
+export const Header: React.FC<HeaderProps> = ({ t }) => {
+  const handleOpenNavbar = async () => {
+    'use server'
+    console.log(123)
   }
-
-  useEffect(() => {
-    if (isOpened) {
-      document.body.classList.add('page__body--with-menu')
-    } else {
-      document.body.classList.remove('page__body--with-menu')
-    }
-  }, [isOpened])
 
   return (
     <>
@@ -49,36 +39,28 @@ export const Header = () => {
                 <LanguageDropdown />
               </div>
               <div className="header__right">
-                <div className="header__logo header__logo_navbar">
-                  <Image
-                    src={isOpened ? crossImage : burgerImage}
-                    alt="burgerImage"
-                    className="header__img"
-                    onClick={handleOpenNavbar}
-                  />
-                </div>
+                <BurgerIcon
+                  src={burgerImage}
+                  handleOpenNavbar={handleOpenNavbar}
+                />
                 <div className="header__navigation-line">
                   <ul className="header__list">
                     <li className="header__item">
-                      <a className="header__link">{t('navbar.navbar.about')}</a>
+                      <a className="header__link">{t.navbar.about}</a>
                     </li>
                     <li className="header__item">
-                      <a className="header__link">{t('navbar.navbar.tech')}</a>
+                      <a className="header__link">{t.navbar.tech}</a>
                     </li>
                     <li className="header__item">
-                      <a className="header__link">
-                        {t('navbar.navbar.benefits')}
-                      </a>
+                      <a className="header__link">{t.navbar.benefits}</a>
                     </li>
                     <li className="header__item">
-                      <a className="header__link">
-                        {t('navbar.navbar.contact')}
-                      </a>
+                      <a className="header__link">{t.navbar.contact}</a>
                     </li>
                   </ul>
                   <Button
                     type="button"
-                    text={t('header.button', 'Buy Now')}
+                    text={t.header.button}
                     classname="header__button header__button_nav"
                   />
                 </div>
@@ -92,25 +74,18 @@ export const Header = () => {
               </article>
               <article className="header__main-content main-block-header__article main-block-header__article_content">
                 <h1 className="header__title">
-                  {t('header.title', 'THE NEW START OF VR LOCOMOTION')}{' '}
-                  <TextDecorator>
-                    {t('header.textDecorator', 'VR LOCOMOTION')}
-                  </TextDecorator>
+                  {t.header.title}{' '}
+                  <TextDecorator>{t.header.textDecorator}</TextDecorator>
                 </h1>
                 <section className="header__text-wrapper">
-                  <p className="header__subtitle">
-                    {t(
-                      'header.subtitle',
-                      'Discover the most comprehensive VR Locomotion system, and unlock infinite motion in any games on any platforms!'
-                    )}
-                  </p>
-                  <p className="header__price">{t('header.price', '1200$')}</p>
+                  <p className="header__subtitle">{t.header.subtitle}</p>
+                  <p className="header__price">{t.header.price}</p>
                   <PlayVideo classname="header__video-play" />
                 </section>
 
                 <Button
                   type="button"
-                  text={t('header.button', 'Buy Now')}
+                  text={t.header.button}
                   classname="header__button header__button_down"
                 />
               </article>
@@ -119,7 +94,7 @@ export const Header = () => {
           </div>
         </Container>
       </header>
-      <Navbar handleOpenNavbar={handleOpenNavbar} isOpened={isOpened} />
+      {/* <Navbar handleOpenNavbar={handleOpenNavbar} isOpened={isOpened} /> */}
     </>
   )
 }
