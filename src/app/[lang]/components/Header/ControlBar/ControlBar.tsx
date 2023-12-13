8ae9adee4419b8a08afdd4bb0fe581d7630327f8
Denys Topchyi <miliarder97@gmail.com>
2023-12-13 23:56:08 +0200

    feat: add usability of modal faq to control bar

--- src/app/[lang]/components/Header/ControlBar/ControlBar.tsx
+++ src/app/[lang]/components/Header/ControlBar/ControlBar.tsx
@@ -1,6 +1,8 @@
 import { Translations } from '@/app/[lang]/types/Translations'
 import './ControlBar.scss'
 import { MoreButton } from './MoreButton/MoreButton'
+import { Modal } from '../../Modal/Modal'
+import { FAQComponent } from '../../FAQ/FAQComponent '
 
 interface ControlBarProps {
   t: Translations
@@ -8,19 +10,24 @@ interface ControlBarProps {
 
 export const ControlBar: React.FC<ControlBarProps> = ({ t }) => {
   return (
-    <section className="header__control-bar control-bar">
-      <article className="control-bar__left">
-        <p className="control-bar__FAQ">{t['control-bar'].faq}</p>
-        <p className="control-bar__help">{t['control-bar'].help}</p>
-      </article>
-      <MoreButton
-        classes="control-bar__center"
-        text={t['control-bar']['more-btn']}
-      />
-      <article className="control-bar__right">
-        <p className="control-bar__previous">{t['control-bar'].previous}</p>
-        <p className="control-bar__next">{t['control-bar'].next}</p>
-      </article>
-    </section>
+    <>
+      <section className="header__control-bar control-bar">
+        <article className="control-bar__left">
+          <p className="control-bar__FAQ">{t['control-bar'].faq}</p>
+          <p className="control-bar__help">{t['control-bar'].help}</p>
+        </article>
+        <MoreButton
+          classes="control-bar__center"
+          text={t['control-bar']['more-btn']}
+        />
+        <article className="control-bar__right">
+          <p className="control-bar__previous">{t['control-bar'].previous}</p>
+          <p className="control-bar__next">{t['control-bar'].next}</p>
+        </article>
+      </section>
+      <Modal isActive={true}>
+        <FAQComponent />
+      </Modal>
+    </>
   )
 }
