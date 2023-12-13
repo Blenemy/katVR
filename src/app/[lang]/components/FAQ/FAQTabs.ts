3013826e5f91cbebc98fb1be80d9f2ba8886efee
Denys Topchyi <miliarder97@gmail.com>
2023-12-13 23:55:28 +0200

    feat: create tabs for FAQ comp

---
+++ src/app/[lang]/components/FAQ/FAQTabs.ts
@@ -0,0 +1,23 @@
+export const faqData = [
+  {
+    question: 'What makes KAT loco different from other systems?',
+    answer:
+      'Despite being approached from many different angles, all traditional VR Locomotion systems including roomscale, teleportation, and free locomotion failed to provide an optimal solution capable of integrating high immersion with convenience. KAT loco does all of that without repeating their flaws. Our system offers a complete and versatile solution for an affordable price what makes it a perfect choice for home-use. It is also 100% user friendly, wireless, universally compatible, and more! It even offers decoupled head and body directions!',
+    lastUpdated: 'Wed, June 12 2019 7:07 PM UTC +03:00'
+  },
+  {
+    question: 'Will it decrease my motion sickness?',
+    answer: 'Text for tab 2',
+    lastUpdated: 'Last updated date here'
+  },
+  {
+    question: 'Which VR headsets is it compatible with?',
+    answer: 'Text for tab 3',
+    lastUpdated: 'Last updated date here'
+  },
+  {
+    question: 'Is it compatible with Oculus Quest?',
+    answer: 'Text for tab 4',
+    lastUpdated: 'Last updated date here'
+  }
+]
