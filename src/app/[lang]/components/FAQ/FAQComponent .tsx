c870acf166cf1205b13856d304e3e42626dffd6b
Denys Topchyi <miliarder97@gmail.com>
2023-12-14 20:47:50 +0200

    ref: ref tabs

--- src/app/[lang]/components/FAQ/FAQComponent .tsx
+++ src/app/[lang]/components/FAQ/FAQComponent .tsx
@@ -8,28 +8,29 @@ import { TextDecorator } from '../TextDecorator/TextDecorator'
 export const FAQComponent = () => {
   const [activeTab, setActiveTab] = useState(0)
 
+  const renderFAQ = (
+    faq: { question: string; answer: string; lastUpdated: string },
+    index: number
+  ) => (
+    <div className="tab-faq" key={index} onClick={() => setActiveTab(index)}>
+      <div className="tab-faq__content">
+        <h4 className="tab-faq__title">{faq.question}</h4>
+        {activeTab === index && (
+          <>
+            <p className="tab-faq__text">{faq.answer}</p>
+            <p className="tab-faq__date">Last updated: {faq.lastUpdated}</p>
+          </>
+        )}
+      </div>
+    </div>
+  )
+
   return (
     <section className="faq">
       <h3 className="faq__title">
         FREQUENTLY ASKED <TextDecorator>QUESTIONS</TextDecorator>
       </h3>
-      {faqData.map((faq, index) => (
-        <div
-          className="tab-faq"
-          key={index}
-          onClick={() => setActiveTab(index)}
-        >
-          <div className="tab-faq__content">
-            <h4 className="tab-faq__title">{faq.question}</h4>
-            {activeTab === index && (
-              <>
-                <p className="tab-faq__text">{faq.answer}</p>
-                <p className="tab-faq__date">Last updated: {faq.lastUpdated}</p>
-              </>
-            )}
-          </div>
-        </div>
-      ))}
+      {faqData.map(renderFAQ)}
     </section>
   )
 }
