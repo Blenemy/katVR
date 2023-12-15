08a16b3d7b71373684a34039c675b303d05fd185
Denys Topchyi <miliarder97@gmail.com>
2023-12-15 13:54:48 +0200

    feat: add default navbar comp

---
+++ src/app/[lang]/components/Navbar/DefaultNavbar/DefaultNavbar.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+
+import cross from '../../../../../../public/icons/cross-white.svg'
+import { Translations } from '@/app/[lang]/types/Translations'
+
+interface DefaultNavbarProps {
+  t: Translations
+  handleOnClick: () => void
+  handleLanguageClick: () => void
+}
+
+export const DefaultNavbar: React.FC<DefaultNavbarProps> = ({
+  t,
+  handleLanguageClick,
+  handleOnClick
+}) => {
+  return (
+    <>
+      <Image
+        src={cross}
+        alt="burgerImage"
+        className="menu__close"
+        onClick={handleOnClick}
+      />
+      <ul className="menu__list">
+        <li className="menu__item">{t.navbar.about}</li>
+        <li className="menu__item">{t.navbar.tech}</li>
+        <li className="menu__item">{t.navbar.benefits}</li>
+        <li className="menu__item">{t.navbar.contact}</li>
+        <li className="menu__item" onClick={handleLanguageClick}>
+          {t.navbar.language}
+        </li>
+        <li className="menu__item">{t.navbar.faq}</li>
+        <li className="menu__item">{t.navbar.help}</li>
+      </ul>
+    </>
+  )
+}
