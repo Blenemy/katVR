17bedd470ae71270ec704385656fd124d14cb7de
Denys Topchyi <miliarder97@gmail.com>
2023-12-17 16:54:26 +0200

    feat: add modal opening on click

--- src/app/[lang]/components/Header/ControlBar/ControlBar.tsx
+++ src/app/[lang]/components/Header/ControlBar/ControlBar.tsx
@@ -1,17 +1,35 @@
+'use client'
+
 import { Translations } from '@/app/[lang]/types/Translations'
-import './ControlBar.scss'
 import { MoreButton } from './MoreButton/MoreButton'
+import { Modal } from '../../Modal/Modal'
+import { FAQComponent } from '../../FAQ/FAQComponent'
+
+import './ControlBar.scss'
+import { useState } from 'react'
 
 interface ControlBarProps {
   t: Translations
 }
 
 export const ControlBar: React.FC<ControlBarProps> = ({ t }) => {
+  const [isActiveModal, setActiveModal] = useState<boolean>(false)
+
+  const handleToggleModal = () => {
+    setActiveModal(false)
+  }
+
+  const handleOpenModal = () => {
+    setActiveModal(true)
+  }
+
   return (
     <>
       <section className="header__control-bar control-bar">
         <article className="control-bar__left">
-          <p className="control-bar__FAQ">{t['control-bar'].faq}</p>
+          <p className="control-bar__FAQ" onClick={handleOpenModal}>
+            {t['control-bar'].faq}
+          </p>
           <p className="control-bar__help">{t['control-bar'].help}</p>
         </article>
         <MoreButton
@@ -23,9 +41,11 @@ export const ControlBar: React.FC<ControlBarProps> = ({ t }) => {
           <p className="control-bar__next">{t['control-bar'].next}</p>
         </article>
       </section>
-      {/* <Modal isActive={true}>
-        <FAQComponent />
-      </Modal> */}
+      {isActiveModal && (
+        <Modal isActive={isActiveModal} handleToggleModal={handleToggleModal}>
+          <FAQComponent t={t} isDesktop />
+        </Modal>
+      )}
     </>
   )
 }
