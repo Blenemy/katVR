9fecc02af5b1259cae0ecbf82b6e29f773192478
Denys Topchyi <miliarder97@gmail.com>
2023-12-17 16:54:00 +0200

    feat: add interactive svg image as component

--- src/app/[lang]/components/Modal/Modal.tsx
+++ src/app/[lang]/components/Modal/Modal.tsx
@@ -1,13 +1,19 @@
 import { ReactNode } from 'react'
 import cn from 'classnames'
+
 import './Modal.scss'
 
 interface ModalProps {
   children: ReactNode
   isActive: boolean
+  handleToggleModal: () => void
 }
 
-export const Modal: React.FC<ModalProps> = ({ children, isActive }) => {
+export const Modal: React.FC<ModalProps> = ({
+  children,
+  isActive,
+  handleToggleModal
+}) => {
   return (
     <div
       className={cn('modal', {
@@ -19,8 +25,23 @@ export const Modal: React.FC<ModalProps> = ({ children, isActive }) => {
           'active-modal__content': isActive
         })}
       >
-        {children}
+        <>
+          <SvgComponent onClick={handleToggleModal} className="modal__close" />
+          {children}
+        </>
       </div>
     </div>
   )
 }
+
+const SvgComponent = (props: any) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={19}
+    height={19}
+    fill="none"
+    {...props}
+  >
+    <path stroke="#fff" d="m1.354.996 16.867 16.867M1.134 17.867 18.001 1" />
+  </svg>
+)
