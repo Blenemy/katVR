154af1695beace8d3b124632f1842b3f47945aaf
Denys Topchyi <miliarder97@gmail.com>
2023-12-18 12:27:13 +0200

    feat: add memo to textdecorator component

--- src/app/[lang]/components/TextDecorator/TextDecorator.tsx
+++ src/app/[lang]/components/TextDecorator/TextDecorator.tsx
@@ -1,17 +1,16 @@
-import { HTMLAttributes } from 'react'
+import { HTMLAttributes, memo } from 'react'
 import './TextDecorator.scss'
 
 interface TextDecoratorProps extends HTMLAttributes<HTMLSpanElement> {
   children: React.ReactNode
 }
 
-export const TextDecorator: React.FC<TextDecoratorProps> = ({
-  children,
-  ...props
-}) => {
-  return (
-    <span className="text-decorator" {...props}>
-      {children}
-    </span>
-  )
-}
+export const TextDecorator: React.FC<TextDecoratorProps> = memo(
+  ({ children, ...props }) => {
+    return (
+      <span className="text-decorator" {...props}>
+        {children}
+      </span>
+    )
+  }
+)
