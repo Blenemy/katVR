199cbf8bf9f9649c1fc27da4d82f78270564dbf9
Denys Topchyi <miliarder97@gmail.com>
2023-12-18 14:04:05 +0200

    feat: add youtube video player to the component

--- src/app/[lang]/components/PlayVideo/PlayVideo.tsx
+++ src/app/[lang]/components/PlayVideo/PlayVideo.tsx
@@ -1,22 +1,50 @@
-import Image from 'next/image'
-import playVideo from '../../../../../public/icons/play-video-active.svg'
-import cn from 'classnames'
+'use client'
 
+import Image from 'next/image'
+import cn from 'classnames'
+import { useState } from 'react'
+import { Modal } from '../Modal/Modal'
+import YouTube from 'react-youtube'
+
+import playVideo from '../../../../../public/icons/play-video-active.svg'
+
+import './PlayVideo.scss'
 interface PlayVideoProps {
   classname: string
-  playVideoFunc: any
 }
 
-type PartialVideoProps = Partial<PlayVideoProps>
+export const PlayVideo: React.FC<PlayVideoProps> = ({ classname }) => {
+  const [isModalOpen, setModalOpen] = useState<boolean>(false)
 
-export const PlayVideo: React.FC<PartialVideoProps> = ({ classname }) => {
+  const handleToggleModal = () => {
+    setModalOpen((prev) => !prev)
+  }
+
+  const opts = {
+    height: '486px',
+    width: '100%',
+    playerVars: {
+      autoplay: 0
+    }
+  }
+
   return (
-    <Image
-      src={playVideo}
-      alt="playVideo"
-      className={cn('play-video', {
-        [`${classname}`]: !!classname
-      })}
-    />
+    <>
+      <Image
+        src={playVideo}
+        alt="playVideo"
+        className={cn('play-video', {
+          [`${classname}`]: !!classname
+        })}
+        onClick={handleToggleModal}
+      />
+      {isModalOpen && (
+        <Modal isActive={isModalOpen} handleToggleModal={handleToggleModal}>
+          <div className="video-wrapper">
+            <YouTube videoId={'sgEJ4sOwboM'} opts={opts} />
+          </div>
+        </Modal>
+      )}
+    </>
   )
 }
