4ca82b0708e2fe021bc6c7a40d983a8c5641d8c8
Denys Topchyi <miliarder97@gmail.com>
2023-12-20 17:45:01 +0200

    feat: add pricebar component

---
+++ src/app/[lang]/components/PriceBar/PriceBar.tsx
@@ -0,0 +1,10 @@
+import './PriceBar.scss'
+
+export const PriceBar = () => {
+  return (
+    <div className="price-bar">
+      <p className="price-bar__title">Price</p>
+      <p className="price-bar__price">1200$</p>
+    </div>
+  )
+}
