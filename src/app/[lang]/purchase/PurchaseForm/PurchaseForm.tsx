b4551d018829ef99d5865fd07a62c01023f62cfa
Denys Topchyi <miliarder97@gmail.com>
2023-12-20 19:30:52 +0200

    feat: implement form handling using formik & yup

--- src/app/[lang]/purchase/PurchaseForm/PurchaseForm.tsx
+++ src/app/[lang]/purchase/PurchaseForm/PurchaseForm.tsx
@@ -1,14 +1,23 @@
+'use client'
+
 import { Button } from '../../components/Button/Button'
 import { CustomInput } from '../../components/CustomInput/CustomInput'
 import { CustomSelect } from '../../components/CustomSelect/CustomSelect'
 import { PriceBar } from '../../components/PriceBar/PriceBar'
+import { usePuchaseForm } from '../../hooks/usePuchaseForm'
+
 import './PurchaseForm.scss'
 
 const OPTIONS = ['1', '2', '3', '4']
 
 export const PurchaseForm = () => {
+  const { formik } = usePuchaseForm()
+
   return (
-    <form className="main-purchase__form form-purchase">
+    <form
+      className="main-purchase__form form-purchase"
+      onSubmit={formik.handleSubmit}
+    >
       <div className="form-purchase__content">
         <section className="form-purchase__price">
           <CustomSelect options={OPTIONS} variant="price" />
@@ -21,6 +30,10 @@ export const PurchaseForm = () => {
             required
             id="First Name"
             label="First Name"
+            onChangeFunc={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values['First Name']}
+            error={formik.touched['First Name'] && formik.errors['First Name']}
           />
           <CustomInput
             name="Last Name"
@@ -28,6 +41,10 @@ export const PurchaseForm = () => {
             required
             id="Last Name"
             label="Last Name"
+            onChangeFunc={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values['Last Name']}
+            error={formik.touched['Last Name'] && formik.errors['Last Name']}
           />
           <CustomInput
             name="Email"
@@ -35,6 +52,10 @@ export const PurchaseForm = () => {
             required
             id="Email"
             label="Email"
+            onChangeFunc={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.Email}
+            error={formik.touched.Email && formik.errors.Email}
           />
           <CustomInput
             name="PhoneNumber"
@@ -42,6 +63,10 @@ export const PurchaseForm = () => {
             required
             id="PhoneNumber"
             label="PhoneNumber"
+            onChangeFunc={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.PhoneNumber}
+            error={formik.touched.PhoneNumber && formik.errors.PhoneNumber}
           />
           <CustomSelect
             options={OPTIONS}
@@ -55,6 +80,13 @@ export const PurchaseForm = () => {
             required
             id="Shipping Adress"
             label="Shipping Adress"
+            onChangeFunc={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values['Shipping Adress']}
+            error={
+              formik.touched['Shipping Adress'] &&
+              formik.errors['Shipping Adress']
+            }
           />
           <CustomInput
             name="Shipping Adress 2"
@@ -62,6 +94,13 @@ export const PurchaseForm = () => {
             id="Shipping Adress 2"
             label="Shipping Adress 2"
             required={false}
+            onChangeFunc={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values['Shipping Adress 2']}
+            error={
+              formik.touched['Shipping Adress 2'] &&
+              formik.errors['Shipping Adress 2']
+            }
           />
           <Button type="submit" text="Purchase" />
         </section>
