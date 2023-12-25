c8cb63f77f125b3c9da1ecb40048fa8a4c834c30
Denys Topchyi <miliarder97@gmail.com>
2023-12-25 20:40:07 +0200

    feat: add multistep functionality to purchase section

--- src/app/[lang]/purchase/PurchaseForm/PurchaseForm.tsx
+++ src/app/[lang]/purchase/PurchaseForm/PurchaseForm.tsx
@@ -2,17 +2,55 @@
 
 import { Button } from '../../components/Button/Button'
 import { CustomSelect } from '../../components/CustomSelect/CustomSelect'
-import { usePuchaseForm } from '../../hooks/usePuchaseForm'
 
 import { PriceBar } from '../../components/PriceBar/PriceBar'
-import { StepTwoForm } from './StepTwoForm/StepTwoForm'
 
 import './PurchaseForm.scss'
+import { StepOneForm } from './StepOneForm/StepOneForm'
+import { useState } from 'react'
+import { StepTwoForm } from './StepTwoForm/StepTwoForm'
+import { useFormik } from 'formik'
+import { purchaseValidationStepOne } from '../../validationSchemas/purchaseValidation_step_one'
 
 const OPTIONS = ['1', '2', '3', '4']
+const BASE_PRICE = 1200
 
 export const PurchaseForm = () => {
-  const { formik } = usePuchaseForm()
+  const [currentStep, setCurrentStep] = useState(0)
+  const [totalValue, setTotalValue] = useState<number>(BASE_PRICE)
+
+  const handleChangeCurrentAmout = (payload: number) => {
+    setTotalValue(BASE_PRICE * payload)
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      'First Name': '',
+      'Last Name': '',
+      Email: '',
+      PhoneNumber: '',
+      'Shipping Adress': '',
+      'Shipping Adress 2': ''
+    },
+    validationSchema: purchaseValidationStepOne,
+    validateOnBlur: true,
+    onSubmit: (values) => {
+      console.log(values)
+    }
+  })
+
+  const handleNextStep = async () => {
+    // Validate current form
+    const errors = await formik.validateForm()
+    if (Object.keys(errors).length === 0) {
+      setCurrentStep(currentStep + 1) // Go to the next step if no errors
+    }
+  }
+
+  const steps = [
+    <StepOneForm formik={formik} key="StepOne" />,
+    <StepTwoForm formik={formik} key="StepTwo" />
+  ]
 
   return (
     <form
@@ -21,11 +59,20 @@ export const PurchaseForm = () => {
     >
       <div className="form-purchase__content">
         <section className="form-purchase__price">
-          <CustomSelect options={OPTIONS} variant="price" />
-          <PriceBar />
+          <CustomSelect
+            options={OPTIONS}
+            variant="price"
+            onChangeFunc={handleChangeCurrentAmout}
+          />
+          <PriceBar totalValue={totalValue} />
         </section>
-        <StepTwoForm formik={formik} />
-        <Button type="submit" text="Purchase" />
+        {steps[currentStep]}
+        <Button
+          type="submit"
+          text={currentStep === 0 ? 'Next Page' : 'Purchase'}
+          classname="form-purchase__button"
+          onClick={handleNextStep}
+        />
       </div>
     </form>
   )
