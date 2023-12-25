0394687fef7bdf36cb69f1373da47ba6f02da42d
Denys Topchyi <miliarder97@gmail.com>
2023-12-25 20:40:48 +0200

    ref: commented shortly unused code

---
+++ src/app/[lang]/hooks/useMultiStepForm.ts
@@ -0,0 +1,7 @@
+// import { useFormik } from 'formik'
+// import { useState } from 'react'
+// import { purchaseValidationStepOne } from '../validationSchemas/purchaseValidation_step_one'
+
+// export const useMultiStepForm = (stepComponents: any[]) => {
+
+// }
