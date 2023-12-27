90fc722d0b30a4523e7a0bc58941413f162d08bd
Denys Topchyi <miliarder97@gmail.com>
2023-12-27 19:36:37 +0200

    feat: change structure logic and made code splitting

--- src/app/[lang]/purchase/PurchaseForm/PurchaseForm.tsx
+++ src/app/[lang]/purchase/PurchaseForm/PurchaseForm.tsx
@@ -1,17 +1,12 @@
-import { memo, useCallback } from 'react'
+import { memo } from 'react'
 
 import { Button } from '../../components/Button/Button'
-import { CustomSelect } from '../../components/CustomSelect/CustomSelect'
-import { PriceBar } from '../../components/PriceBar/PriceBar'
 import { StepOneForm } from './StepOneForm/StepOneForm'
 import { StepTwoForm } from './StepTwoForm/StepTwoForm'
 
 import './PurchaseForm.scss'
 import { FormikProps } from 'formik'
 import { PurchaseFormValues } from '../../types/PurchaseForm'
-
-const OPTIONS = ['1', '2', '3', '4']
-const BASE_PRICE = 1200
 
 interface PurchaseFormProps {
   currentStep: number
@@ -24,33 +19,12 @@ export const PurchaseForm: React.FC<PurchaseFormProps> = memo(
     // TODO: add last purchase-form component
     const StepComponent = [StepOneForm, StepTwoForm][currentStep]
 
-    const totalValue = formik.values.quantity * BASE_PRICE
-
-    const handleSelectChange = useCallback(
-      (name: string) => (value: string) => {
-        formik.setFieldValue(name, value)
-      },
-      [formik]
-    )
-
     return (
       <form
         className="main-purchase__form form-purchase"
         onSubmit={formik.handleSubmit}
       >
         <div className="form-purchase__content">
-          <section className="form-purchase__price">
-            <CustomSelect
-              options={OPTIONS}
-              variant="price"
-              name="country"
-              onChangeFunc={handleSelectChange('quantity')}
-              value={formik.values.quantity}
-              error={formik.touched.quantity && formik.errors.quantity}
-              label="Quantity"
-            />
-            <PriceBar totalValue={totalValue} />
-          </section>
           <StepComponent formik={formik} />
           <Button
             type="submit"
