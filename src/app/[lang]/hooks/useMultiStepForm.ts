401590284b35de9b5f3c472b0cf443ff8c34a72e
Denys Topchyi <miliarder97@gmail.com>
2023-12-27 13:11:38 +0200

    ref: moved multistepfrom logic into separate hook

--- src/app/[lang]/hooks/useMultiStepForm.ts
+++ src/app/[lang]/hooks/useMultiStepForm.ts
@@ -1,7 +1,35 @@
-// import { useFormik } from 'formik'
-// import { useState } from 'react'
-// import { purchaseValidationStepOne } from '../validationSchemas/purchaseValidation_step_one'
+import { useFormik } from 'formik'
+import { useState } from 'react'
+import { purchaseValidationStepOne } from '../validationSchemas/purchaseValidation_step_one'
 
-// export const useMultiStepForm = (stepComponents: any[]) => {
+export const useMultiStepForm = () => {
+  const [currentStep, setCurrentStep] = useState(0)
 
-// }
+  const formik = useFormik({
+    initialValues: {
+      quantity: 1,
+      'First Name': '',
+      'Last Name': '',
+      Email: '',
+      PhoneNumber: '',
+      'Shipping Adress': '',
+      'Shipping Adress 2': '',
+      country: 'Choose a country',
+      city: 'Choose a city'
+    },
+    validationSchema: purchaseValidationStepOne,
+    validateOnBlur: true,
+    onSubmit: (values) => {
+      console.log(values)
+    }
+  })
+
+  const handleNextStep = async () => {
+    const errors = await formik.validateForm()
+    if (Object.keys(errors).length === 0) {
+      setCurrentStep(currentStep + 1)
+    }
+  }
+
+  return { formik, handleNextStep, currentStep }
+}
