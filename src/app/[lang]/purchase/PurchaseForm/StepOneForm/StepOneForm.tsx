28e9265cfc641505feee1a716892e132c9ba2e92
Denys Topchyi <miliarder97@gmail.com>
2024-01-01 19:51:28 +0200

    fix: changed architecture of first step

--- src/app/[lang]/purchase/PurchaseForm/StepOneForm/StepOneForm.tsx
+++ src/app/[lang]/purchase/PurchaseForm/StepOneForm/StepOneForm.tsx
@@ -1,128 +1,157 @@
 import { CustomInput } from '@/app/[lang]/components/CustomInput/CustomInput'
 import { CustomSelect } from '@/app/[lang]/components/CustomSelect/CustomSelect'
-import { PurchaseFormValues } from '@/app/[lang]/types/PurchaseForm'
-import { FormikProps } from 'formik'
-import { useCallback } from 'react'
+import { Button } from '@/app/[lang]/components/Button/Button'
+import { Form, Formik } from 'formik'
+import { TPurchaseData } from '@/app/[lang]/types/PurchaseData'
+import { purchaseValidationStepOne } from '@/app/[lang]/validationSchemas/purchaseValidationStepOne'
 
 import './StepOneForm.scss'
 
-const OPTIONS_CONTRIES = ['Ukraine', 'Poland', 'Great Britain', 'Canada', 'USA']
-const OPTIONS_CITIES = ['Kyiv', 'Warsaw', 'London', 'Ottawa', 'Washington']
+const OPTIONS_CONTRIES: string[] = [
+  'Ukraine',
+  'Poland',
+  'Great Britain',
+  'Canada',
+  'USA'
+]
+const OPTIONS_CITIES: string[] = [
+  'Kyiv',
+  'Warsaw',
+  'London',
+  'Ottawa',
+  'Washington'
+]
 
-export const StepOneForm = ({
-  formik
-}: {
-  formik: FormikProps<PurchaseFormValues>
-}) => {
-  const handleSelectChange = useCallback(
-    (name: string) => (value: string) => {
-      formik.setFieldValue(name, value)
-    },
-    [formik]
-  )
+interface IStepOneFormProps {
+  next: (newData: Partial<TPurchaseData>) => void
+  data: TPurchaseData
+}
+
+export const StepOneForm: React.FC<IStepOneFormProps> = ({ next, data }) => {
+  const handleSubmit = (values: Partial<TPurchaseData>) => {
+    next(values)
+  }
 
   return (
-    <>
-      <section className="step-one-purchase-form">
-        <CustomInput
-          name="First Name"
-          type="text"
-          required
-          id="First Name"
-          label="First Name"
-          onChangeFunc={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values['First Name']}
-          error={formik.touched['First Name'] && formik.errors['First Name']}
-          classname="step-one-purchase-form__field"
-        />
-        <CustomInput
-          name="Last Name"
-          type="text"
-          required
-          id="Last Name"
-          label="Last Name"
-          onChangeFunc={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values['Last Name']}
-          error={formik.touched['Last Name'] && formik.errors['Last Name']}
-          classname="step-one-purchase-form__field"
-        />
-        <CustomInput
-          name="Email"
-          type="email"
-          required
-          id="Email"
-          label="Email"
-          onChangeFunc={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.Email}
-          error={formik.touched.Email && formik.errors.Email}
-          classname="step-one-purchase-form__field"
-        />
-        <CustomInput
-          name="PhoneNumber"
-          type="tel"
-          required
-          id="PhoneNumber"
-          label="PhoneNumber"
-          onChangeFunc={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.PhoneNumber}
-          error={formik.touched.PhoneNumber && formik.errors.PhoneNumber}
-          classname="step-one-purchase-form__field"
-        />
-        <CustomSelect
-          options={OPTIONS_CONTRIES}
-          variant="formField"
-          label="Country*"
-          name="country"
-          onChangeFunc={handleSelectChange('country')}
-          value={formik.values.country}
-          error={formik.touched.country && formik.errors.country}
-          classname="step-one-purchase-form__field"
-        />
-        <CustomSelect
-          options={OPTIONS_CITIES}
-          variant="formField"
-          label="City*"
-          name="city"
-          onChangeFunc={handleSelectChange('city')}
-          value={formik.values.city}
-          error={formik.touched.city && formik.errors.city}
-          classname="step-one-purchase-form__field"
-        />
-        <CustomInput
-          name="Shipping Adress"
-          type="text"
-          required
-          id="Shipping Adress"
-          label="Shipping Adress"
-          onChangeFunc={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values['Shipping Adress']}
-          error={
-            formik.touched['Shipping Adress'] &&
-            formik.errors['Shipping Adress']
-          }
-          classname="step-one-purchase-form__field"
-        />
-        <CustomInput
-          name="Shipping Adress 2"
-          type="text"
-          id="Shipping Adress 2"
-          label="Shipping Adress 2"
-          required={false}
-          onChangeFunc={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values['Shipping Adress 2']}
-          error={
-            formik.touched['Shipping Adress 2'] &&
-            formik.errors['Shipping Adress 2']
-          }
-          classname="step-one-purchase-form__field"
-        />
-      </section>
-    </>
+    <Formik
+      initialValues={data}
+      onSubmit={handleSubmit}
+      validationSchema={purchaseValidationStepOne}
+    >
+      {({
+        handleChange,
+        handleBlur,
+        values,
+        touched,
+        errors,
+        setFieldValue
+      }) => {
+        const handleSelectChange = (name: string) => (value: string) => {
+          setFieldValue(name, value)
+        }
+
+        return (
+          <Form className="step-one-purchase-form">
+            <CustomInput
+              name="firstName"
+              type="text"
+              required
+              id="firstName"
+              label="First Name"
+              onChangeFunc={handleChange}
+              onBlur={handleBlur}
+              value={values.firstName}
+              error={touched.firstName && errors.firstName}
+              classname="step-one-purchase-form__field"
+            />
+            <CustomInput
+              name="lastName"
+              type="text"
+              required
+              id="lastName"
+              label="Last Name"
+              onChangeFunc={handleChange}
+              onBlur={handleBlur}
+              value={values.lastName}
+              error={touched.lastName && errors.lastName}
+              classname="step-one-purchase-form__field"
+            />
+            <CustomInput
+              name="email"
+              type="email"
+              required
+              id="email"
+              label="Email"
+              onChangeFunc={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+              error={touched.email && errors.email}
+              classname="step-one-purchase-form__field"
+            />
+            <CustomInput
+              name="phone"
+              type="tel"
+              required
+              id="phone"
+              label="PhoneNumber"
+              onChangeFunc={handleChange}
+              onBlur={handleBlur}
+              value={values.phone}
+              error={touched.phone && errors.phone}
+              classname="step-one-purchase-form__field"
+            />
+            <CustomSelect
+              options={OPTIONS_CONTRIES}
+              variant="formField"
+              label="Country*"
+              name="country"
+              onChangeFunc={handleSelectChange('country')}
+              value={values.country}
+              error={touched.country && errors.country}
+              classname="step-one-purchase-form__field"
+            />
+            <CustomSelect
+              options={OPTIONS_CITIES}
+              variant="formField"
+              label="City*"
+              name="city"
+              onChangeFunc={handleSelectChange('city')}
+              value={values.city}
+              error={touched.city && errors.city}
+              classname="step-one-purchase-form__field"
+            />
+            <CustomInput
+              name="shippingAdress"
+              type="text"
+              required
+              id="shippingAdress"
+              label="Shipping Adress"
+              onChangeFunc={handleChange}
+              onBlur={handleBlur}
+              value={values.shippingAdress}
+              error={touched.shippingAdress && errors.shippingAdress}
+              classname="step-one-purchase-form__field"
+            />
+            <CustomInput
+              name="shippingAdress2"
+              type="text"
+              id="shippingAdress2"
+              label="Shipping Adress 2"
+              required={false}
+              onChangeFunc={handleChange}
+              onBlur={handleBlur}
+              value={values.shippingAdress2}
+              error={touched.shippingAdress2 && errors.shippingAdress2}
+              classname="step-one-purchase-form__field"
+            />
+            <Button
+              type="submit"
+              text="Next page"
+              classname="form-purchase__button"
+            />
+          </Form>
+        )
+      }}
+    </Formik>
   )
 }
