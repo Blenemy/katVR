b1a2835c6e8c74224090aba6af5e2b2661017aca
Denys Topchyi <miliarder97@gmail.com>
2024-01-01 19:52:06 +0200

    fix: changed step two

--- src/app/[lang]/purchase/PurchaseForm/StepTwoForm/StepTwoForm.tsx
+++ src/app/[lang]/purchase/PurchaseForm/StepTwoForm/StepTwoForm.tsx
@@ -1,63 +1,80 @@
 import { CardInput } from '@/app/[lang]/components/CardInput/CardInput'
 import { CustomInput } from '@/app/[lang]/components/CustomInput/CustomInput'
-import { PurchaseFormValues } from '@/app/[lang]/types/PurchaseForm'
-import { FormikProps } from 'formik'
+import { Form, Formik } from 'formik'
 
 import './StepTwoForm.scss'
+import { purchaseValidationStepTwo } from '@/app/[lang]/validationSchemas/purchaseValidationStepTwo'
+import { TPurchaseData } from '@/app/[lang]/types/PurchaseData'
+import { Button } from '@/app/[lang]/components/Button/Button'
 
-export const StepTwoForm = ({
-  formik
-}: {
-  formik: FormikProps<PurchaseFormValues>
-}) => {
+interface IStepTwoFormProps {
+  next: (newData: Partial<TPurchaseData>) => void
+  data: TPurchaseData
+}
+
+export const StepTwoForm: React.FC<IStepTwoFormProps> = ({ next, data }) => {
+  const handleSubmit = (values: Partial<TPurchaseData>) => {
+    next(values)
+    console.log('clown')
+  }
+
   return (
-    <section className="step-two-purchase-form">
-      <CardInput required label="Card Number" />
-      <CustomInput
-        name="Card Holder Name"
-        type="text"
-        required
-        id="Card Holder Name"
-        label="Card Holder Name"
-        onChangeFunc={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values['Card Holder Name']}
-        error={
-          formik.touched['Card Holder Name'] &&
-          formik.errors['Card Holder Name']
-        }
-        classname="step-two-purchase-form__field"
-      />
-      <section className="step-two-purchase-form__credentials">
-        <CustomInput
-          name="Expiration Date"
-          type="text"
-          required
-          inputType="expiration-date"
-          placeholder="MM / YY"
-          id="Expiration Date"
-          label="Expiration Date"
-          onChangeFunc={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values['Expiration Date']}
-          error={
-            formik.touched['Expiration Date'] &&
-            formik.errors['Expiration Date']
-          }
-        />
-        <CustomInput
-          name="CVV"
-          type="text"
-          required
-          placeholder="000"
-          id="CVV"
-          label="CVV"
-          onChangeFunc={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.CVV}
-          error={formik.touched.CVV && formik.errors.CVV}
-        />
-      </section>
-    </section>
+    <Formik
+      initialValues={data}
+      onSubmit={handleSubmit}
+      validationSchema={purchaseValidationStepTwo}
+    >
+      {({ values, handleChange, handleBlur, errors, touched }) => {
+        return (
+          <Form className="step-two-purchase-form">
+            <CardInput required label="Card Number" />
+            <CustomInput
+              name="cardHolder"
+              type="text"
+              required
+              id="cardHolder"
+              label="Card Holder Name"
+              onChangeFunc={handleChange}
+              onBlur={handleBlur}
+              value={values.cardHolder}
+              error={touched.cardHolder && errors.cardHolder}
+              classname="step-two-purchase-form__field"
+            />
+            <section className="step-two-purchase-form__credentials">
+              <CustomInput
+                name="expirationDate"
+                type="text"
+                required
+                inputType="expiration-date"
+                placeholder="MM / YY"
+                id="expirationDate"
+                label="Expiration Date"
+                onChangeFunc={handleChange}
+                onBlur={handleBlur}
+                value={values.expirationDate}
+                error={touched.expirationDate && errors.expirationDate}
+              />
+              <CustomInput
+                name="CVV"
+                type="text"
+                required
+                placeholder="000"
+                id="CVV"
+                label="CVV"
+                onChangeFunc={handleChange}
+                onBlur={handleBlur}
+                value={values.CVV}
+                error={touched.CVV && errors.CVV}
+              />
+            </section>
+            <Button
+              type="submit"
+              text="Purchase"
+              classname="form-purchase__button"
+            />
+          </Form>
+        )
+      }}
+    </Formik>
   )
 }
