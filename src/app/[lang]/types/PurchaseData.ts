461dc9da510ef9687f743ab3d0989eda39f6d07f
Denys Topchyi <miliarder97@gmail.com>
2024-01-01 19:50:59 +0200

    fix: changed field names to proper values

---
+++ src/app/[lang]/types/PurchaseData.ts
@@ -0,0 +1,15 @@
+export type TPurchaseData = {
+  quantity: number,
+  firstName: string
+  lastName: string
+  email: string
+  phone: string
+  shippingAdress: string
+  shippingAdress2: string
+  country: string
+  city: string
+  cardNumber: string
+  cardHolder: string
+  expirationDate: string
+  CVV: string
+}
