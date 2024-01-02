07777e75a84b20a2b1d5f5123332f6fb386ccb31
Denys Topchyi <miliarder97@gmail.com>
2024-01-02 19:02:26 +0200

    feat: add last step to the multistepform

--- src/app/[lang]/purchase/PurchaseLayout/PurchaseMainContent/PurchaseMainContent.tsx
+++ src/app/[lang]/purchase/PurchaseLayout/PurchaseMainContent/PurchaseMainContent.tsx
@@ -5,21 +5,25 @@ import { PurchaseNavigation } from '../../PurchaseNavigation/PurchaseNavigation'
 import { PurchaseForm } from '../../PurchaseForm/PurchaseForm'
 
 import './PurchaseMainContent.scss'
+import { StepThreeForm } from '../../PurchaseForm/StepThreeForm/StepThreeForm'
 
 export const PurchaseMainContent = () => {
-  const { currentStep, currentElement, data, setData } =
-    useMultiStepForm()
+  const { currentStep, currentElement, data, setData } = useMultiStepForm()
 
   return (
     <section className="purchase__main main-purchase">
       <PurchaseNavigation currentStep={currentStep} />
       <PurchaseNavigation currentStep={currentStep} design="Desktop" />
-      <PurchaseForm
-        currentStep={currentStep}
-        currentFormElement={currentElement}
-        data={data}
-        setData={setData}
-      />
+      {currentStep < 2 ? (
+        <PurchaseForm
+          currentStep={currentStep}
+          currentFormElement={currentElement}
+          data={data}
+          setData={setData}
+        />
+      ) : (
+        <StepThreeForm />
+      )}
     </section>
   )
 }
