770a9fd9927cecd17bb4919dadd52b6916488e71
Denys Topchyi <miliarder97@gmail.com>
2024-01-06 20:27:35 +0200

    fix: adjust pricebar to accept translation

--- src/app/[lang]/components/PriceBar/PriceBar.tsx
+++ src/app/[lang]/components/PriceBar/PriceBar.tsx
@@ -1,15 +1,19 @@
 import { memo } from 'react'
+
 import './PriceBar.scss'
 
 interface PriceBarProps {
   totalValue: number
+  text: string
 }
 
-export const PriceBar: React.FC<PriceBarProps> = memo(({ totalValue }) => {
-  return (
-    <div className="price-bar">
-      <p className="price-bar__title">Price</p>
-      <p className="price-bar__price">{totalValue}$</p>
-    </div>
-  )
-})
+export const PriceBar: React.FC<PriceBarProps> = memo(
+  ({ totalValue, text }) => {
+    return (
+      <div className="price-bar">
+        <p className="price-bar__title">{text}</p>
+        <p className="price-bar__price">{totalValue}$</p>
+      </div>
+    )
+  }
+)
