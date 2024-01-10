9866324abf4407e675f13db7595df90f5dbe204f
Denys Topchyi <miliarder97@gmail.com>
2024-01-10 19:21:23 +0200

    feat: add proper validation to the first step

--- src/app/[lang]/validationSchemas/purchaseValidationStepOne.ts
+++ src/app/[lang]/validationSchemas/purchaseValidationStepOne.ts
@@ -1,19 +1,25 @@
 import * as Yup from 'yup'
 
 export const purchaseValidationStepOne = Yup.object({
-  firstName: Yup.string().required(),
-  lastName: Yup.string().required(),
+  firstName: Yup.string()
+    .required('Please, fill your name*')
+    .min(3, 'At least 3 characters'),
+  lastName: Yup.string()
+    .required('Please, fill your last name*')
+    .min(3, 'At least 3 characters'),
   email: Yup.string()
-    // .matches(/^[a-zA-Z0-9._-]+@([a-zA-Z]+[-])*[a-zA-Z]+(\.[a-zA-Z]{2,4})+$/)
-    .required(),
-  phone: Yup.string().required(),
-  // .max(16, 'Невірний формат телефону. Правильний формат: +380#########')
-  // .matches(
-  //   /^\+380\d{2}\d{3}\d{4}$/,
-  //   'Невірний формат номеру. Приклад: +380#########'
-  // ),
-  shippingAdress: Yup.string().required(),
-  shippingAdress2: Yup.string(),
+    .matches(
+      /^[a-zA-Z0-9._-]+@([a-zA-Z]+[-])*[a-zA-Z]+(\.[a-zA-Z]{2,4})+$/,
+      'Invalid email format'
+    )
+    .required('Please, fill your email*'),
+  phone: Yup.string()
+    .matches(/^\+?\d{10,15}$/, 'Invalid phone number format')
+    .required('Phone number is required'),
+  shippingAdress: Yup.string()
+    .required('Please specify your adress')
+    .max(30, 'Maximum 30 characters'),
+  shippingAdress2: Yup.string().max(30, 'Maximum 30 characters'),
   country: Yup.string()
     .notOneOf(['Choose a country'], 'You must select a country')
     .required('Country is required'),
