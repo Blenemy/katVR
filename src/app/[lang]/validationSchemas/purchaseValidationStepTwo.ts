4ddcacb924ae05bb94314f4e52feb7479c603da6
Denys Topchyi <miliarder97@gmail.com>
2024-01-11 21:36:10 +0200

    feat: add validation the the second form

--- src/app/[lang]/validationSchemas/purchaseValidationStepTwo.ts
+++ src/app/[lang]/validationSchemas/purchaseValidationStepTwo.ts
@@ -5,10 +5,15 @@ const segmentValidator = Yup.string()
   .required('Each segment is required')
 
 export const purchaseValidationStepTwo = Yup.object({
-  cardHolder: Yup.string().required(),
+  cardHolder: Yup.string()
+    .required()
+    .min(3, 'Cardholders name should be at least 3 characters'),
   cardNumber: Yup.array()
     .of(segmentValidator)
     .length(4, 'Card number must have exactly 4 segments'),
   expirationDate: Yup.string().required(),
-  CVV: Yup.string().required()
+  CVV: Yup.string()
+    .required()
+    .min(3, 'At least 3 characters')
+    .max(3, 'Maximum 3 characters')
 })
